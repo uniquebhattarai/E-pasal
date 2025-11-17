6c3b1f8527bf0f83048c15b6250e8252eb05de08
Unique Bhattarai <uniquebhattarai@Uniques-MacBook-Air.local>
2025-11-17 12:07:45 +0545

    wip

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,31 +1,27 @@
 const nodemailer = require("nodemailer");
 
-const mailSender = async (email, title, body) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",   
-      port: 587,                
-      secure: false,            
-      auth: {
-        user: process.env.MAIL_USER, 
-        pass: process.env.MAIL_PASS, 
-      },
-    });
+const mailSender = async(email,title,body)=>{
+    try {
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS,
+            }
+        })
 
-    const info = await transporter.sendMail({
-      from: process.env.MAIL_USER,
-      to: email,
-      subject: title,
-      html: body,
-    });
+        let info = await transporter.sendMail({
+            from:'e-pasal',
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body}`,
+        });
+        console.log(info);
+        return info;
 
-    console.log("Mail sent:", info.messageId);
-    return info;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-  } catch (error) {
-    console.error("MAIL ERROR:", error);
-    throw error;
-  }
-};
-
-module.exports = mailSender;
+module.exports=mailSender;
