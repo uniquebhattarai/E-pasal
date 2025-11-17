feb598e6f2432ea732b0bb61eff1245902e291c8
Unique Bhattarai <uniquebhattarai@Uniques-MacBook-Air.local>
2025-11-17 10:48:35 +0545

    mail fixed

--- server/controller/resetPassword.js
+++ server/controller/resetPassword.js
@@ -1,95 +1,109 @@
-
 const User = require("../models/user");
 const mailSender = require("../utils/mailSender");
 const bcrypt = require("bcrypt");
-require("dotenv").config();
+const crypto = require("crypto");
 
-exports.resetPasswordToken= async (req,res)=>{
-   try {
-   
-     const email = req.body.email;
-     
-     const user = await User.findOne({email:email});
-     if(!user){
-         return res.status(400).json({
-             success:false,
-             message:"user doesnot exist"
-         });
-     }
-  
-     const token = crypto.randomUUID();
-  
-     const updatedDetails = await User.findOneAndUpdate({email:email},{
-         token:token,
-         resetPasswordExpires:Date.now()+5*60*1000
-     },{
-         new:true
-     });
- 
-     const url =`${process.env.FRONTEND_URL}/update-password/${token}`
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-     await mailSender(email,"Password reset link",`password reset link:${url}`);
-    
-     return res.json({
-         success:true,
-         message:"E-mail sent successfully, please check email and change password",
-     });
- 
-   } catch (error) {
-    console.error(error)
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    const email = req.body.email;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+
+    const token = crypto.randomUUID();
+
+
+    await User.findOneAndUpdate(
+      { email },
+      {
+        token,
+        resetPasswordExpires: Date.now() + 5 * 60 * 1000, // 5 minutes
+      },
+      { new: true }
+    );
+
+    const url = `${process.env.FRONTEND_URL}/update-password/${token}`;
+
+    await mailSender(email, "Password Reset Link", `
+      <p>Click the link below to reset your password:</p>
+      <a href="${url}">${url}</a>
+    `);
+
+    return res.json({
+      success: true,
+      message: "Email sent successfully. Check your inbox.",
+    });
+
+  } catch (error) {
+    console.error(error);
     return res.status(500).json({
-        success:fail,
-        message:"Error while reseting password"
+      success: false,
+      message: "Error while generating reset token",
     });
-   }
-    
-}
+  }
+};
 
 
+exports.resetPassword = async (req, res) => {
+  try {
+    const { password, confirmPassword, token } = req.body;
 
-//resetpassword
-
-exports.resetPassword= async (req,res)=>{
-    try {
-        //data fetch
-    const {password,confirmPassword,token}=req.body;
-    //validation
-    if(password !== confirmPassword){
-        return res.json({
-            success:false,
-            message:"Password Do not match",
-        });
+    if (password !== confirmPassword) {
+      return res.json({
+        success: false,
+        message: "Passwords do not match",
+      });
     }
-    //get user details from db using token
-    const userDetails = await User.findOne({token:token});
-    //if no entry invalid token
-    if(!userDetails){
-        return res.json({
-            success:false,
-            message:"Token is invalid"
-        });
+
+    // Check user with token
+    const userDetails = await User.findOne({ token });
+
+    if (!userDetails) {
+      return res.json({
+        success: false,
+        message: "Invalid token",
+      });
     }
-    //check token time
-    if(userDetails.resetPasswordExpires < Date.now()){
-        return res.json({
-            success:false,
-            message:"Token is expired,please regenerate your token",
-        });
+
+    if (userDetails.resetPasswordExpires < Date.now()) {
+      return res.json({
+        success: false,
+        message: "Token expired. Try again.",
+      });
     }
-    //hash password
-    const hashedPassword = await bcrypt.hash(password,10);
-    //update the password
-    await User.findOneAndUpdate({token:token},{password:hashedPassword},{new:true});
-    //return response
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.findOneAndUpdate(
+      { token },
+      {
+        password: hashedPassword,
+        token: null,
+        resetPasswordExpires: null,
+      },
+      { new: true }
+    );
+
     return res.status(200).json({
-        success:true,
-        message:"Password reset sucessful",
+      success: true,
+      message: "Password reset successful",
     });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success:false,
-            message:"Error while reseting password",
-        });
-    }
-}
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error while resetting password",
+    });
+  }
+};
--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,24 +2,24 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST || "smtp.gmail.com",
-      port: 587,               
-      secure: false,           
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",   
+      port: 587,                
+      secure: false,            
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        user: process.env.MAIL_USER, 
+        pass: process.env.MAIL_PASS, 
       },
     });
 
-    let info = await transporter.sendMail({
-      from: process.env.MAIL_USER,  
+    const info = await transporter.sendMail({
+      from: process.env.MAIL_USER,
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("MAIL SENT:", info.messageId);
+    console.log("Mail sent:", info.messageId);
     return info;
 
   } catch (error) {
